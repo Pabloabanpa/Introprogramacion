dbcaa47ed270899eff4907cf99e6d0a34790a4c2
HP 240 <pabloaban123@gmail.com>
2020-09-18 17:06:30 -0400

    añadir distancia tierra luna

--- calculador distancia luna.py
+++ calculador distancia luna.py
@@ -1,4 +1,4 @@
 distancia_tierra_luna = 384400
-distancia_recorrida = 50000
+distancia_recorrida = int(input("por favor ingrese distancia recorrida")
 distancia_restante = distancia_tierra_luna - distancia_recorrida
 print(distancia_restante)
