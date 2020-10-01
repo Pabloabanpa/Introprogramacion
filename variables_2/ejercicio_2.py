4598404c9880270edc9a07d71d2f2a7bda68d46f
HP 240 <pabloaban123@gmail.com>
2020-10-01 17:15:07 -0400

    añadir distancia tierra luna 1

---
+++ variables_2/ejercicio_2.py
@@ -0,0 +1,4 @@
+dividendo = int(input("por favor,introduzca el numero dividendo:"))
+divisor = int(input ("por favor, introduca el número divisor:"))
+resto = int(dividendo % divisor)
+respuesta = int(print("el resto de la opercaión" , {dividendo} , "entre" , {divisor} +"es:" , {resto})
