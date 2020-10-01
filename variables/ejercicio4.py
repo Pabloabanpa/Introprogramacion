661c31e49cf3e364a04d0123164192e976dba4e0
HP 240 <pabloaban123@gmail.com>
2020-10-01 17:10:10 -0400

    Por favor, introduce tu nombre

--- variables/ejercicio4.py
+++ variables/ejercicio4.py
@@ -1,6 +1,6 @@
 frase1 = ("Albert Enistein, dijo unavez")
-frase2 = ("\cada dia sabemos ma y entendemos menos"\")
-mensaje = frase1 + frase
+frase2 = "\"cada dia sabemos ma y entendemos menos"
+mensaje = (frase1 , frase)
 
 
 
---
+++ variables_2/ejrcicio4.py
@@ -0,0 +1,10 @@
+num1 = int(input("ingrese el priner número"))
+num2 = int(input("ingrese el segundo nu´mero"))
+son_iguales= num1 == num2
+print(f"los números son iguales: {son_iguales} ")
+son_distintos = num1 != num2
+print(f"los números son distontos: {son_distintos}")
+num1_mayor_que_num2 = num1 > num2
+print(f"num1 es mayor que num2: {num1_mayor_que_num2}")
+num1_menor_o_igual_que_num2 = num1 <= num2
+print(f"num1 es menor o igual que num2: {num1_menor_o_igual_que_num2}")
