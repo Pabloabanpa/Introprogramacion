52618205ba5f4a5446fd08afeec0d39e31cbf8c1
HP 240 <pabloaban123@gmail.com>
2020-10-08 16:39:34 -0400

    añade un número

---
+++ practico_1/ejercicio_1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Escriba un número: "))
+if num1%2==0 :
+    print("su número es par.")
+
+elif  num1%2!=0 :
+    print("Su número es impar")
+
+elif num1==0  :
+    print("Su número es igual a 0")
