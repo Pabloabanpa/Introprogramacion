0b1b807117a8167492adb2b7486f6011ef2e3fd1
HP 240 <pabloaban123@gmail.com>
2020-10-09 15:00:03 -0400

    añade un número

---
+++ practico_1/ejercicio_5.py
@@ -0,0 +1,11 @@
+
+palabra_1 = input("Escriba una palabra: ")
+palabra_2 = input("Escriba otra palbra: ")
+
+if palabra_1>palabra_2:
+    print((palabra_1),":es mas larga ya que tiene",len(palabra_1),"caracteres")
+
+elif palabra_1<palabra_2:
+    print((palabra_2),":es mas larga ya que tiene",len(palabra_2),"caracteres")
+
+
