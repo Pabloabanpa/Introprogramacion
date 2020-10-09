8006f80c1391add49353cd060133e9034a9cb4ca
HP 240 <pabloaban123@gmail.com>
2020-10-09 16:39:33 -0400

    ingrese un número

--- practico_1/ejercicio_5.py
+++ practico_1/ejercicio_5.py
@@ -2,10 +2,12 @@
 palabra_1 = input("Escriba una palabra: ")
 palabra_2 = input("Escriba otra palbra: ")
 
-if palabra_1>palabra_2:
-    print((palabra_1),":es mas larga ya que tiene",len(palabra_1),"caracteres")
+if len(palabra_1)>len(palabra_2):
+    print((palabra_1),":es la palabra mas larga ya que tiene",len(palabra_1),"caracteres")
 
-elif palabra_1<palabra_2:
-    print((palabra_2),":es mas larga ya que tiene",len(palabra_2),"caracteres")
+elif len(palabra_1)<len(palabra_2):
+    print((palabra_2),":es la palabra mas larga ya que tiene",len(palabra_2),"caracteres")
 
+else :
+    print("Las dos palabras tienen la misma longitud")
 
