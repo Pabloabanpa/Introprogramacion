a0df3c9d7607bc08f1e57708c61ba089bfeb9f85
Pabloabanpa <71466135+Pabloabanpa@users.noreply.github.com>
2020-10-22 16:40:45 -0400

    Add files via upload
    TABLAS DE MULTIPLICAR

---
+++ tablas_multiplicar.py
@@ -0,0 +1,36 @@
+#Tablas ed multiplicar
+def tablaM(numI):
+    for i in range (1,11):
+        print(numI,"x",i,"=",i*numI)
+
+num=1
+num1=2
+num2=3
+num3=4
+num4=5
+num5=6
+num6=7
+num7=8
+num8=9
+num9=10
+print("Tabla del 1:")
+tablaM(num)
+print("Tabla del 2:")
+tablaM(num1)
+print("Tabla del 3:")
+tablaM(num2)
+print("Tabla del 4:")
+tablaM(num3)
+print("Tabla del 5:")
+tablaM(num4)
+print("Tabla del 6:")
+tablaM(num5)
+print("Tabla del 7:")
+tablaM(num6)
+print("Tabla del 8:")
+tablaM(num7)
+print("Tabla del 9:")
+tablaM(num8)
+print("Tabla del 10:")
+tablaM(num9)
+print("FIN")
